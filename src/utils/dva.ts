3648804dc63e79db4708cfed48d11eea9c2526c4
李先森 <1440955662@qq.com>
2020-08-16 22:37:43 +0800

    store

--- src/utils/dva.ts
+++ src/utils/dva.ts
@@ -1,15 +1,43 @@
+import { create } from 'dva-core';
 import { message } from 'antd';
+import createLoading from 'dva-loading';
 
-export function config() {
-  return {
-    onError(err: any) {
-      err.preventDefault();
-      message.error(err.message);
-    },
-    initialState: {
-      global: {
-        text: 'hi umi + dva',
-      },
-    },
+let app: {
+  use: (val: any) => void;
+  model: (val: any) => any;
+  start: () => void;
+  _store: { dispatch: any };
+  getStore: () => { dispatch: any };
+  dispatch: any;
+};
+let store: { dispatch: any };
+let dispatch;
+
+function createApp(opt: { onError: (err: any) => void; models: any[] }) {
+  // redux日志
+  // opt.onAction = [createLogger()];
+  opt.onError = (err: any) => {
+    console.error(err);
+    // Taro.hideLoading();
+    message.error('服务器错误');
   };
+  app = create(opt);
+  app.use(createLoading({}));
+  opt.models.forEach((model) => app.model(model.default));
+  app.start();
+
+  store = app._store;
+  app.getStore = () => store;
+
+  dispatch = store.dispatch;
+
+  app.dispatch = dispatch;
+  return app;
 }
+
+export default {
+  createApp,
+  getDispatch() {
+    return app.dispatch;
+  },
+};
