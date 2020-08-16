d9e5152963cac8981aac8835bdbd84bfd4a52fd0
李先森 <1440955662@qq.com>
2020-08-16 22:35:36 +0800

    输出

--- src/global.tsx
+++ src/global.tsx
@@ -1,3 +1,12 @@
 import '@/assets/iconfont/icon.scss';
 import '@/assets/scss/reset.scss';
 import '@/assets/scss/layout.scss';
+import models from './models/index';
+import dva from './utils/dva';
+const dvaApp = dva.createApp({
+  initialState: {},
+  models,
+});
+
+const store = dvaApp.getStore();
+console.log(store, '------>');
